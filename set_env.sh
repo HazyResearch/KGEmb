f1003a7be06fa7fb5e5f13df787885ecaccc27c5
ines-chami <ines-chami@users.noreply.github.com>
2020-05-04 14:42:35 +0000

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -3,5 +3,4 @@ KGHOME=$(pwd)
 export PYTHONPATH="$KGHOME:$PYTHONPATH"
 export LOG_DIR="$KGHOME/logs"
 export DATA_PATH="$KGHOME/data"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64
 source hyp_kg_env/bin/activate
