8eba4bf923ec14e04870161c5ac306a0deff49e3
ines-chami <ines-chami@users.noreply.github.com>
2020-05-04 14:40:40 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 """Train Knowledge Graph embeddings for link prediction."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import argparse
 import json
 import logging
